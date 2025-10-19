5439e63b136e4700b36e46e394d3f5c04d2b9941
arantzaghg <arantzaghg@gmail.com>
2025-10-19 14:54:55 -0600

    Implement get_portfolio_value function to calculate total portfolio value based on cash, long, and short positions

---
+++ portfolio_value.py
@@ -0,0 +1,16 @@
+from models import Operation
+
+def get_portfolio_value(cash: float, long_ops: list[Operation], short_ops: list[Operation], current_price: float) -> float:
+
+    port_val = cash
+
+    # add long positions value
+    for position in long_ops:
+        port_val += current_price * position.n_shares
+
+   # add short positions value
+    for position in short_ops:
+        pnl = (position.price - current_price) * position.n_shares
+        port_val += pnl
+
+    return port_val
