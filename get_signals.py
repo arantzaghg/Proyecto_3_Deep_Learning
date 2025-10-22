3f6e1f92ad7e1186609e83b6a22b9be6c73d4524
AlexFajardo26 <javier.fajardo@iteso.mx>
2025-10-21 23:44:23 -0600

    Fix pct_change calculation to use a 5-period window and ensure NaN values are dropped correctly

--- get_signals.py
+++ get_signals.py
@@ -3,12 +3,13 @@ import pandas as pd
 def signals(data: pd.DataFrame) -> pd.DataFrame:
 
     data = data.copy()
-    data['pct_change'] = data['Close'].pct_change(fill_method=None).dropna()
+    data['pct_change'] = data['Close'].pct_change(periods=5)
+    data.dropna(inplace=True)
 
 
     data["signal"] = 0
-    data.loc[data["pct_change"] > 0.008, "signal"] = 1
-    data.loc[data["pct_change"] < -0.008, "signal"] = 2
+    data.loc[data["pct_change"] > 0.03, "signal"] = 1
+    data.loc[data["pct_change"] < -0.03, "signal"] = 2
 
     data.drop(columns=["pct_change"], inplace=True)
     data["signal"] = data["signal"].astype(int)
