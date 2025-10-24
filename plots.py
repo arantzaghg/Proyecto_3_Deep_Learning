7e678829f2cc12489c5e4bd419211b1ba68f98e0
arantzaghg <arantzaghg@gmail.com>
2025-10-23 20:52:27 -0600

    Refactor plot_portfolio_value function to include title parameter for improved plot labeling and clarity.

--- plots.py
+++ plots.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_portfolio_value(portfolio_value) :
+def plot_portfolio_value(portfolio_value, title: str):
 
     '''
     Plot the portfolio value over the training period.
